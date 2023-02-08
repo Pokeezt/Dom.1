05d57283556ec799f72ab1d48f2b3fa2e6c0bebe
Pokeezt <100492131+Pokeezt@users.noreply.github.com>
2023-02-08 20:47:02 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+const standingsBody = document.getElementById("standings-body");
+const matchesBody = document.getElementById("matches-body");
+
+// Aqui estão alguns exemplos de times
+const teams = {
+  { name: "Time A", wins: 0, losses: 0, draws: 0, points: 0 },
+  { name: "Time B", wins: 0, losses: 0, draws: 0, points: 0 },
+  { name: "Time C", wins: 0, losses: 0, draws: 0, points: 0 },
+  // ... e mais times
+};
+
+// Aqui estão alguns exemplos de partidas
+const matches = {
+  { time1: "Time A", result: "Vitória", time2: "Time B" },
+  { time1: "Time B", result: "Derrota", time2: "Time C" },
+
+};
+//isso é todos, aqui Kauê Brás :P, 
